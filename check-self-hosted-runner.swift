8489b2b4fe42f67cb0aa3e087d7f7f177f4d42e1
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-29 22:13:07 -0700

    cancel workflow when locked

--- check-self-hosted-runner.swift
+++ check-self-hosted-runner.swift
@@ -4,16 +4,15 @@ import Quartz
 
 func checkIfScreenIsLocked() {
     if let session = CGSessionCopyCurrentDictionary() as NSDictionary? {
-        print(session)
+        print("::debug::Quartz GUI window server session dictionary", session)
         if let locked = session["CGSSessionScreenIsLocked"] {
-            print("screen locked: ", locked)
+            print("screen is locked: ", locked)
             print("::warning ::Not ready to run as screen is locked")
             print("::set-output name=screen-locked::true")
         } else {
             print("::set-output name=screen-locked::false")
         }
     }
-    print("::set-output name=screen-locked::true")
 }
 
 checkIfScreenIsLocked()
