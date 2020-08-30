a5f6755b2d5daf50a88188ea652724b3a4b591ff
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-29 21:30:22 -0700

    cancel only when needed

--- cancel-this-run.sh
+++ cancel-this-run.sh
@@ -2,4 +2,4 @@
 
 url=https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/cancel
 echo $url
-curl -X POST -H 'Accept: application/vnd.github.v3+json' -H "authorization: Bearer $GITHUB_TOKEN" $url
+curl -s -o /dev/null -w "%{http_code}" -X POST -H 'Accept: application/vnd.github.v3+json' -H "authorization: Bearer $GITHUB_TOKEN" $url
