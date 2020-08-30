964fd38ad9da4a22860a2b06d23c6a6404cde20b
Paul Zabelin <paulz@users.noreply.github.com>
2020-08-29 21:32:32 -0700

    use env for cancel parameter

--- cancel-this-run.sh
+++ cancel-this-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 url=https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID/cancel
+echo "Should cancel this run: $CANCEL"
 echo $url
 curl -s -o /dev/null -w "%{http_code}" -X POST -H 'Accept: application/vnd.github.v3+json' -H "authorization: Bearer $GITHUB_TOKEN" $url
